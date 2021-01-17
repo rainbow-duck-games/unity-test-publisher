9453e1416621029428c5448762c7992be665b99f
Vladimir Kryukov <erlioniel@gmail.com>
2021-01-17 12:46:52 +0200

    Remove test report and add test project

---
+++ unity-project/Assets/Tests/SamplePlayModeTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+  public class SamplePlayModeTest {
+    [Test]
+    public void PassedTest() {
+      Assert.True(true);
+    }
+    
+    [Test]
+    public void FailedTest() {
+      Assert.True(false);
+    }
+
+    [UnityTest]
+    public IEnumerator PassedUnityTest() {
+      yield return null;
+      Assert.True(true);
+    }
+
+    [UnityTest]
+    public IEnumerator FailedUnityTest() {
+      yield return null;
+      Assert.True(false);
+    }
+  }
+}
