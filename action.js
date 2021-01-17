af76b496857127c1bfa674b2471d5991caa233b9
Vladimir Kryukov <erlioniel@gmail.com>
2021-01-17 13:53:45 +0200

    Add publish check to pull request

--- action.js
+++ action.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const fs = require('fs');
 const xmljs = require('xml-js');
 
@@ -18,8 +19,50 @@ let action = async function (path, githubToken, failOnFailedTests = false, failI
         return;
     }
 
+    // TODo
+    const annotations = [];
+    annotations.push({
+        path: 'unity-project/Assets/Tests/SamplePlayModeTest.cs',
+        start_line: 7,
+        end_line: 9,
+        annotation_level: 'failure',
+        title: 'Test failed stuff',
+        message: 'Test failed message',
+        raw_details: 'RAW ME PLS'
+    });
+
     let results = `${meta.result}: tests: ${meta.total}, skipped: ${meta.skipped}, failed: ${meta.failed}`;
     core.info(results);
+
+    const pullRequest = github.context.payload.pull_request;
+    const link = (pullRequest && pullRequest.html_url) || github.context.ref;
+    const conclusion = meta.total > 0 || !failIfNoTests ? 'success' : 'failure';
+    const status = 'completed';
+    const head_sha = (pullRequest && pullRequest.head.sha) || github.context.sha;
+    core.info(
+        `Posting status '${status}' with conclusion '${conclusion}' to ${link} (sha: ${head_sha})`
+    );
+
+    const createCheckRequest = {
+        ...github.context.repo,
+        name,
+        head_sha,
+        status,
+        conclusion,
+        output: {
+            results,
+            summary: '',
+            annotations: annotations.slice(0, 50)
+        }
+    };
+
+    core.debug(JSON.stringify(createCheckRequest, null, 2));
+
+    // make conclusion consumable by downstream actions
+    core.setOutput('conclusion', conclusion);
+
+    const octokit = new github.GitHub(githubToken);
+    await octokit.checks.create(createCheckRequest);
 
     if (failOnFailedTests && meta.result !== 'Passed') {
         core.setFailed(`There were ${meta.failed} failed tests`);
