96730803699a0d933a8b70ca8338395d73ed4b3e
Vladimir Kryukov <erlioniel@gmail.com>
2021-01-17 10:43:43 +0200

    Test report; Basic input parameters; A lot of todo-s

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Create a JavaScript Action
 
+[comment]: <ToDo>
+
 <p align="center">
   <a href="https://github.com/actions/javascript-action/actions"><img alt="javscript-action status" src="https://github.com/actions/javascript-action/workflows/units-test/badge.svg"></a>
 </p>
---
+++ action.js
@@ -0,0 +1,30 @@
+let action = function (milliseconds) {
+  // ToDo
+  return new Promise((resolve) => {
+    if (typeof milliseconds !== 'number') {
+      throw new Error('milliseconds not a number');
+    }
+    setTimeout(() => resolve("done!"), milliseconds)
+  });
+};
+
+module.exports = action;
+
+//
+// // most @actions toolkit packages have async methods
+// async function run() {
+//     try {
+//         const ms = core.getInput('milliseconds');
+//         core.info(`Waiting ${ms} milliseconds ...`);
+//
+//         core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+//         await wait(parseInt(ms));
+//         core.info((new Date()).toTimeString());
+//
+//         core.setOutput('time', new Date().toTimeString());
+//     } catch (error) {
+//         core.setFailed(error.message);
+//     }
+// }
+//
+// run();
